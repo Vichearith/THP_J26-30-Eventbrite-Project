611c2374de0708c577093a371cbed39da3772cad
Vichearith <vichearith.ros@gmail.com>
2019-08-12 17:59:47 +0200

    Attendance after_create

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,10 @@
 class Attendance < ApplicationRecord
   after_create :new_attendee_send
+  
   belongs_to :user
   belongs_to :event
+
+  def welcome_send
+    AttendanceMailer.new_attendee_email(self).deliver_now
+  end
 end
